be9ec9ef89a78678de0d86245ee225003ee8258c
GRPilot <rusan.rusik@yandex.ru>
2020-11-22 09:06:51 +0300

    Second task

---
+++ GRS_Task_02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GRS_Task_02 {
+    class Program {
+        static void Main(string[] args) {
+            string cmd = "";
+            do {
+                Console.Write("Введите команду: ");
+                cmd = Console.ReadLine();
+            } while(cmd.ToLower() != "exit");
+
+            /* Or we can do:
+             while(true) {
+                Console.Write("Введите команду: ");
+                cmd = Console.ReadLine();
+
+                if(cmd.ToLower() == "exit") {
+                    break;
+                }
+             }
+
+             But this method is disgust. Usually when we wanna exit from a loop,
+             we need make the correct logic for our program and correct condition
+             */
+        }
+    }
+}
