4b0864821a887ff77d68ef7c7fc207ec81e933af
GRPilot <rusan.rusik@yandex.ru>
2020-11-22 09:07:15 +0300

    3th task

---
+++ GRS_Task_03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GRS_Task_3 {
+    class Program {
+        static void Main(string[] args) {
+            const string password = "123456";
+            const int MAX_TRIES = 3;
+
+            string userPass;
+            bool isPassCorrect = false;
+            for(int cur_try = MAX_TRIES - 1; cur_try >= 0; --cur_try) {
+                Console.Write("Введите пароль: ");
+                userPass = Console.ReadLine();
+
+                if(userPass == password) {
+                    isPassCorrect = true;
+                    break;
+                }
+                Console.WriteLine("Неверно введен пароль! Осталось попыток: {0}", cur_try);
+            }
+
+            if(isPassCorrect) {
+                Console.WriteLine("Секретное сообщение: \"Пароль был {0}\"", password);
+            }
+        }
+    }
+}
