60e2f36df343c5be86b9d2864ccb42d59cd5e28f
GRPilot <rusan.rusik@yandex.ru>
2020-11-25 09:59:28 +0300

    Complete GRS_Task_06

--- GRS_Task_06/Program.cs
+++ GRS_Task_06/Program.cs
@@ -20,8 +20,17 @@ namespace GRS_Task_06 {
         }
 
         static void Main(string[] args) {
-            string[] fullnames = new string[0];
-            string[] states = new string[0];
+            string[] fullnames = new string[3];
+            string[] states = new string[3];
+            fullnames[0] = "Головинский Руслан Сергеевич";
+            fullnames[1] = "Баранов Константин Павлович";
+            fullnames[2] = "Творун Даниил -----";
+            states[0] = "Программист C++";
+            states[1] = "Программист";
+            states[2] = "Full-stack разработчик";
+
+            const int minWindowWidth = 133;
+            Console.WindowWidth = minWindowWidth;
 
             ID id;
             do {
@@ -41,6 +50,7 @@ namespace GRS_Task_06 {
                         break;
                 }
                 if(id != ID.EXIT) {
+                    Console.SetCursorPosition(0, fullnames.Length + 10);
                     Console.WriteLine("Нажмите любую клавишу.");
                     Console.ReadKey(true);
                 }
@@ -120,7 +130,6 @@ namespace GRS_Task_06 {
                         break;
 
                     case ConsoleKey.Enter:
-                        Console.SetCursorPosition(0, menuCount + 7);
                         return ++selected;
                 }
 
@@ -218,14 +227,14 @@ namespace GRS_Task_06 {
 
         static void ShowDossiers(ref string[] fullname, ref string[] state) {
             const int idBlockSize = 5;
-            const int nameBlockSize = 50;
-            const int stateBlockSize = 26;
+            const int nameBlockSize = 90;
+            const int stateBlockSize = 28;
             string line = "║{0}{1} │{2}{3}│{4}{5}║";
 
             Console.Clear();
-            Console.WriteLine("╔══════╤══════════════════════════════════════════════════╤══════════════════════════╗");
-            Console.WriteLine("║  id  │               Фамилия Имя Отчество               │        Должность         ║");
-            Console.WriteLine("╠══════╪══════════════════════════════════════════════════╪══════════════════════════╣");
+            Console.WriteLine("╔══════╤══════════════════════════════════════════════════════════════════════════════════════════╤════════════════════════════╗");
+            Console.WriteLine("║  id  │                                   Фамилия Имя Отчество                                   │         Должность          ║");
+            Console.WriteLine("╠══════╪══════════════════════════════════════════════════════════════════════════════════════════╪════════════════════════════╣");
 
             if(fullname.Length > 0) { 
                 for(int i = 0; i < fullname.Length; ++i) {
@@ -240,22 +249,21 @@ namespace GRS_Task_06 {
                     Console.WriteLine(line, id_spacer, i + 1, fullname[i], name_spacer, state[i], state_spacer);
                 }
             } else { 
-                Console.WriteLine("║      │               Записи отсутствуют                 │                          ║");
+                Console.WriteLine("║      │                                   Записи отсутствуют                                     │                            ║");
             }
 
-            Console.WriteLine("╚══════╧══════════════════════════════════════════════════╧══════════════════════════╝");
+            Console.WriteLine("╚══════╧══════════════════════════════════════════════════════════════════════════════════════════╧════════════════════════════╝");
         }
 
         static void RemoveDossier(ref string[] fullname, ref string[] state) {
             Console.Clear();
             if(fullname.Length == 0) {
-                MsgBox("Внимание! Досье отсутствуют!", "Добавьте досье в главном|меню");
+                MsgBox("Внимание! Досье отсутствуют!", "Добавьте досье в главном|меню.");
                 return;
             }
 
             const int idFieldSize = 5;
             const int nameFieldSize = 39;
-            string line = "║{0}{1} │ {2}{3} ║";
 
             // showing 
 
@@ -269,7 +277,8 @@ namespace GRS_Task_06 {
                             "╠══════╪═══════════════════════════════════════╣";
 
             string footer = "╠══════╧═══════════════════════════════════════╣\n" +
-                            "║ Для выбора нажмите Enter                     ║\n" +
+                            "║ Для выбора нажмите Enter.                    ║\n" +
+                            "║ Для выхода в главное меню нажмите Esc.       ║\n" +
                             "╚══════════════════════════════════════════════╝";
             string open =  "║";
             string close = "║";
@@ -332,12 +341,9 @@ namespace GRS_Task_06 {
                         Console.Write(close);
 
                         break;
-
-                    case ConsoleKey.Enter:
-                        Console.SetCursorPosition(0, menuCount + 7);
-                        break;
+                    case ConsoleKey.Escape:
+                        return;
                 }
-
             } while(key.Key != ConsoleKey.Enter);
 
             Array.Copy(fullname, selected + 1, fullname, selected, fullname.Length - selected - 1);
@@ -347,7 +353,91 @@ namespace GRS_Task_06 {
             Array.Resize(ref state, state.Length - 1);
         }
 
-        static void FindByLastName(ref string[] fullname, ref string[] state) { 
+        static void FindByLastName(ref string[] fullname, ref string[] state) {
+            Console.Clear();
+            if(fullname.Length == 0) {
+                MsgBox("Внимание!", "В хранилище записи отсутствуют.|Добавьте досье в главном|меню.");
+                return;
+            }
+
+            const int minSearchColumn = 35;
+            const int maxSearchColumn = 130;
+            const int nameFieldSize = 90;
+            const int stateFieldSize = 39;
+            int maxLineCount = fullname.Length;
+
+            string header = "╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗\n" +
+                            "║ Введите фамилию в строку поиска:                                                                                                 ║\n" +
+                            "╠══════════════════════════════════════════════════════════════════════════════════════════╤═══════════════════════════════════════╣\n" +
+                            "║                                           ФИО                                            │               Должность               ║\n" +
+                            "╠══════════════════════════════════════════════════════════════════════════════════════════╪═══════════════════════════════════════╣";
+
+            string footer = "╠══════════════════════════════════════════════════════════════════════════════════════════╧═══════════════════════════════════════╣\n" +
+                            "║ Для выхода в главное меню нажмите клавишу Esc,                                                                                   ║\n" +
+                            "╚══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝";
+            string format = "║ {0}{1} │ {2}{3} ║";
+
+            Console.WriteLine(header);
+            for(int i = 0; i < maxLineCount; ++i) {
+                string name_spacer = new string(' ', nameFieldSize - fullname[i].Length - 2);
+                string state_spacer = new string(' ', stateFieldSize - state[i].Length - 2);
+                Console.WriteLine(format, fullname[i], name_spacer, state[i], state_spacer);
+            }
+            Console.WriteLine(footer);
+
+            Position defaultPosition = new Position(5, 0);
+
+            Position cursorPosition = new Position(1, minSearchColumn);
+            Console.SetCursorPosition(cursorPosition.column, cursorPosition.row);
+            string search = "";
+            ConsoleKeyInfo key;
+            do {
+                key = Console.ReadKey(true);
+                switch(key.Key) {
+                    case ConsoleKey.Backspace:
+                        if(cursorPosition.column == minSearchColumn) {
+                            continue;
+                        }
+                        --cursorPosition.column;
+                        Console.SetCursorPosition(cursorPosition.column, cursorPosition.row);
+                        Console.Write(' ');
+                        Console.SetCursorPosition(cursorPosition.column, cursorPosition.row);
+                        if (search.Length > 0) {
+                            search = search.Remove(search.Length - 1);
+                        }
+                        break;
+                    default:
+                        if(cursorPosition.column != maxSearchColumn) {
+                            ++cursorPosition.column;
+                            search += key.KeyChar;
+                            Console.Write(key.KeyChar);
+                        }
+                        break;
+                }
+
+                Console.SetCursorPosition(defaultPosition.column, defaultPosition.row);
+                int foundPos = 0;
+                for(int i = 0; i < maxLineCount; ++i) {
+                    int spacePos = fullname[i].IndexOf(' ');
+                    if(spacePos == -1) {
+                        spacePos = fullname[i].Length;
+                    }
+                    if(!fullname[i].Substring(0, spacePos).Contains(search)) {
+                        continue;
+                    }
+                    ++foundPos;
+                    string name_spacer = new string(' ', nameFieldSize - fullname[i].Length - 2);
+                    string state_spacer = new string(' ', stateFieldSize - state[i].Length - 2);
+                    Console.WriteLine(format, fullname[i], name_spacer, state[i], state_spacer);
+                }
+                for(int i = foundPos; i < maxLineCount; ++i) {
+                    string name_spacer = new string(' ', nameFieldSize - 3);
+                    string state_spacer = new string(' ', stateFieldSize - 3);
+                    Console.WriteLine(format, ' ', name_spacer, ' ', state_spacer);
+                }
+                Console.SetCursorPosition(cursorPosition.column, cursorPosition.row);
+
+            } while (key.Key != ConsoleKey.Escape);
         }
 
         static void MsgBox(string prompt, string text) {
